e553cb72f13f9a9e4bb5cd0d3dfed5a1cac4b29b
Maddest-Lad <samharris23@live.com>
2023-12-15 22:23:28 -0800

    implemented notifications

--- go.mod
+++ go.mod
@@ -3,7 +3,15 @@ module src
 go 1.20
 
 require (
-	golang.org/x/image v0.14.0
 	github.com/fsnotify/fsnotify v1.7.0
-	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/image v0.14.0
+)
+
+require (
+	github.com/gen2brain/beeep v0.0.0-20230907135156-1a38885a97fc // indirect
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	golang.org/x/sys v0.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,46 +4,28 @@ import (
 	"fmt"
 	"image/png"
 	"log"
+	"math"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
+	"github.com/gen2brain/beeep"
 	"golang.org/x/image/webp"
 )
 
 func main() {
-	// Create new watcher.
+	// Create New Watcher
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to create new watcher: %s", err)
 	}
 	defer watcher.Close()
 
-	// Listen For Events.
-	go func() {
-		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return
-				}
-				if event.Has(fsnotify.Create) && pathIsWebP(event.Name) {
-					log.Printf("Converting %s to a PNG", event.Name)
-					if err := convertToPNG(event.Name); err != nil {
-						log.Printf("Conversion error: %s", err)
-					} else {
-						log.Printf("Successfully converted %s to a PNG\n", event.Name)
-					}
-				}
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return
-				}
-				log.Print("error:", err)
-			}
-		}
-	}()
+	// Start Listening For Events
+	go dedupLoop(watcher)
 
 	// Add Directories to Watch
 	homeDirectory, err := os.UserHomeDir()
@@ -65,15 +47,87 @@ func main() {
 		log.Printf("failed to watch %s %s", pictures, err)
 	}
 
-	// Block main goroutine forever.
+	// Block forever
+	fmt.Println("WebP Converter Online; Press CTRL+C to Exit")
 	<-make(chan struct{})
+}
+
+// Deduplicates Events For The Same File
+func dedupLoop(w *fsnotify.Watcher) {
+	var (
+		// Wait 250ms for new events; each new event resets the timer.
+		waitFor = 250 * time.Millisecond
+
+		// Keep track of the timers, as path → timer.
+		mu     sync.Mutex
+		timers = make(map[string]*time.Timer)
+
+		// Callback we run.
+		convertToPngEvent = func(event fsnotify.Event) {
+
+			// Check for WebP Files
+			if pathIsWebP(event.Name) {
+				// Convert to PNG
+				if err := convertToPNG(event.Name); err != nil {
+					log.Printf("Conversion error: %s", err)
+				} else {
+					log.Printf("Successfully converted %s to a PNG\n", event.Name)
+
+					if err := sendNotification("Converted"+event.Name, "WebP Converter"); err != nil {
+						log.Printf("Notification error: %s", err)
+					}
+				}
+			}
+
+			// Don't need to remove the timer if you don't have a lot of files.
+			mu.Lock()
+			delete(timers, event.Name)
+			mu.Unlock()
+		}
+	)
+
+	for {
+		select {
+		// Read from Errors.
+		case _, ok := <-w.Errors:
+			if !ok { // Channel was closed
+				return
+			}
+		// Read from Events.
+		case event, ok := <-w.Events:
+			if !ok { // Channel was closed
+				return
+			}
+
+			// Only Observe File Creation and Writes
+			if !event.Has(fsnotify.Create) && !event.Has(fsnotify.Write) {
+				continue
+			}
+
+			// Get timers for the event
+			mu.Lock()
+			t, ok := timers[event.Name]
+			mu.Unlock()
+
+			// No timer yet, so create one.
+			if !ok {
+				t = time.AfterFunc(math.MaxInt64, func() { convertToPngEvent(event) })
+				t.Stop()
+
+				mu.Lock()
+				timers[event.Name] = t
+				mu.Unlock()
+			}
+
+			// Reset the timer for this path, so it will start from 100ms again.
+			t.Reset(waitFor)
+		}
+	}
 }
 
 func pathIsWebP(path string) bool {
 	ext := filepath.Ext(path)
-	ext = strings.TrimPrefix(ext, ".")
-	fmt.Println(path, ext)
-	return ext == "webp"
+	return ext == ".webp"
 }
 
 func convertToPNG(path string) error {
@@ -107,4 +161,8 @@ func convertToPNG(path string) error {
 		return fmt.Errorf("png encode failed: %v", err)
 	}
 	return nil
+}
+
+func sendNotification(message, title string) error {
+	return beeep.Notify(title, message, "resources/icons/icon.png")
 }
